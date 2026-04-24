b6ac4346f6ff6eaced7562e7fc3f2bc0b40ad694
lbachromano <109762221+lbachromano@users.noreply.github.com>
2026-04-24 15:18:50 +0200

    Update run_const_external_rate.sh

--- simulations/run_const_external_rate.sh
+++ simulations/run_const_external_rate.sh
@@ -1,13 +1,18 @@
-n=10
-master_seed=235
+ #!/usr/bin/env bash
 
-nu_ext=2000
-g=3.625
+external_input=0.9 #in the paper, value of nu_{ext}/\nu_{theta}
+homogeneous=0 # 1 to simulate a network with spatially homogeneous connectivity, 1 to simulate a spatially extended network.
+anisotropy_EE=0.7 # rho_EE defines the anisotropy in the E to E connectivity
+n=1 # number of simulations
+trials=1 #number of trials per simulations (each one simulates 1 second of activity)
+master_seed=33  # single seed for reproducibility
+
 for ((i=1; i<=n; i++)); do
     # Derive a unique, reproducible seed for each run
-    rs=$(( master_seed + i*10 ))
+    rs=$(( master_seed + i ))
+
     echo "Running simulation i=$i with seed rs=$rs"
-    python Simulations_sigmoidal_external_rate.py "$i" "$rs" "$nu_ext" "$g"
+    python Simulations_const_ext_rate.py "$i" "$rs" "$trials" "$homogeneous" "$external_input" "$anisotropy_EE"
     echo "Waiting 2 minutes before next run..."
     sleep 120
 done
